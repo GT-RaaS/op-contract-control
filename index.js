41124eee4d7286b5590a125f85d4930f06e25967
touchwave <touchwave@outlook.com>
2024-05-27 14:33:15 +0800

    transferOwnership

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const tSysmtemConfig = require('./tests/tSystemConfig.js')
 const tSuperchainConfig = require('./tests/tSuperchainConfig.js')
+const transferOwnership = require('./tests/t_OwnershipTransfer.js')
 async function main() {
     // console.log("\n===== test SysmtemConfig =====")
     // await tSysmtemConfig()
     
     // console.log("\n===== test SuperchainConfig =====")
     // await tSuperchainConfig()
+
+    console.log("\n===== test OwnershipTransfer =====")
+    await transferOwnership()
 }
 
 main()
